478e76b01cdb76bde51ca7d9414d721ebf454be8
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-03-29 04:27:08 -0400

    add npm run hook to parse IP addr before each serve

--- src/helpers/api-config.js
+++ src/helpers/api-config.js
@@ -1,16 +1,5 @@
 const port = 4000;
 
-const ip = (function () {
-    const ifaces = require('os').networkInterfaces();
-    let r = '127.0.0.1';
-
-    Object.keys(ifaces).forEach(function (ifname) {
-        ifaces[ifname].forEach(function (iface) {
-            if ('IPv4' === iface.family && iface.internal === false)
-                r = iface.address;
-        });
-    });
-    return r;
-})();
+const ip = '127.0.0.1';
 
 export const apiUrl = 'http://' + ip + ':' + port;
